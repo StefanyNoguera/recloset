a627fde91a2ac1c893116cfbc6de6ddc52be67fd
Stefany Noguera <stefanynoguerar@gmail.com>
2026-02-12 13:54:51 +0100

    Add item detail route

--- market/urls.py
+++ market/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, item_detail
 
 urlpatterns = [
     path("", home, name="home"),
+    path("items/<int:pk>/", item_detail, name="item_detail")
 ]
