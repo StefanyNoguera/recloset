a3522975f09a064269b692292c86be52a6b8035f
Stefany Noguera <stefanynoguerar@gmail.com>
2026-02-12 13:51:46 +0100

    Add item detail view

--- market/views.py
+++ market/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 
 def home(request):
     items = Item.objects.filter(is_available=True).order_by("-created_at")
     return render(request, "market/home.html", {"items": items})
+
+def item_detail(request, pk):
+    item = get_object_or_404(Item, pk=pk, is_available=True)
+    return render(request, "market/item_detail.html", {"item": item})
