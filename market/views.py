c8f8fc050dd3aa632c77ff822424dbce44aef727
Stefany Noguera <stefanynoguerar@gmail.com>
2026-02-12 13:38:15 +0100

    Add homepage item feed view and template

--- market/views.py
+++ market/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Item
 
-# Create your views here.
+def home(request):
+    items = Item.objects.filter(is_available=True).order_by("-created_at")
+    return render(request, "market/home.html", {"items": items})
