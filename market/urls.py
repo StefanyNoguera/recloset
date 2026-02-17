02c549376f4771c85489c32e0066f65c8611c7b6
Stefany Noguera <stefanynoguerar@gmail.com>
2026-02-17 13:53:55 +0100

    Add store detail route under /tiendas/

--- market/urls.py
+++ market/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, item_detail
+from .views import home, item_detail, store_detail
 
 urlpatterns = [
     path("", home, name="home"),
-    path("items/<int:pk>/", item_detail, name="item_detail")
+    path("productos/<int:pk>/", item_detail, name="item_detail"),
+    path("tiendas/<int:pk>/", store_detail, name="store_detail")
 ]
