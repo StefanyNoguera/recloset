8dd6452b35d9674402e5ad07dc4d1c18fbad0be8
Stefany Noguera <stefanynoguerar@gmail.com>
2026-02-18 12:31:00 +0100

    Add route for Mi tienda page

--- market/urls.py
+++ market/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, item_detail, store_detail, whatsapp_redirect
+from .views import home, item_detail, store_detail, whatsapp_redirect, my_store
 
 urlpatterns = [
     path("", home, name="home"),
     path("productos/<int:pk>/", item_detail, name="item_detail"),
     path("productos/<int:pk>/whatsapp", whatsapp_redirect, name="whatsapp_redirect"),
     path("tiendas/<int:pk>/", store_detail, name="store_detail"),
+    path("mi-tienda/", my_store, name="my_store"),
 ]
