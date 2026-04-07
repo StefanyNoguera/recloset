a3fc071d9389c158cf63bdd7c90e4cd251d18c44
Stefany Noguera <stefanynoguerar@gmail.com>
2026-04-07 14:35:39 +0200

    Use custom password reset templates for auth views

--- recloset/urls.py
+++ recloset/urls.py
@@ -17,12 +17,50 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.auth import views as auth_views
+
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("cuentas/", include("django.contrib.auth.urls")),
+    path(
+        "cuentas/recuperar/",
+        auth_views.PasswordResetView.as_view(
+            template_name="market/auth/password_reset_form.html",
+            email_template_name="market/auth/password_reset_email.txt",
+            subject_template_name="market/auth/password_reset_subject.txt",
+        ),
+        name="password_reset",
+    ),
+    path(
+        "cuentas/recuperar/enviado/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="market/auth/password_reset_done.html",
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "cuentas/reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="market/auth/password_reset_confirm.html",
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "cuentas/reset/completo/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="market/auth/password_reset_complete.html",
+        ),
+        name="password_reset_complete",
+    ),
+    path(
+        "cuentas/reset/completo/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="registration/password_reset_complete.html",
+        ),
+        name="password_reset_complete",
+    ),
     path("", include("market.urls")),
 ]
 
